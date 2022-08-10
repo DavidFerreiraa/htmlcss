bb000f66e9c426c588e804e75e61b8e78922eeb6
David <dfl8@aluno.ifal.edu.br>
2022-08-10 16:53:48 -0300

    Criação do botão e adição dos ids nos inputs

---
+++ desafios/projeto_cordel/public/js/notas.js
@@ -0,0 +1,33 @@
+// Notas de Matemática
+nota1mat = document.querySelector("#nota1mat")
+nota2mat = document.querySelector("#nota2mat")
+mediamat = document.querySelector("#mediamat")
+
+// Notas de Física
+nota1fis = document.querySelector("#nota1fis")
+nota2fis = document.querySelector("#nota2fis")
+mediafis = document.querySelector("#mediafis")
+
+// Notas de Química
+nota1qui = document.querySelector("#nota1qui")
+nota2qui = document.querySelector("#nota2qui")
+mediaqui = document.querySelector("#mediaqui")
+
+// Notas de Biologia
+nota1bio = document.querySelector("#nota1bio")
+nota2bio = document.querySelector("#nota2bio")
+mediabio = document.querySelector("#mediabio")
+
+// Notas de História
+nota1hist = document.querySelector("#nota1hist")
+nota2hist = document.querySelector("#nota2hist")
+mediahist = document.querySelector("#mediahist")
+
+// Notas de Geografia
+nota1geo = document.querySelector("#nota1geo")
+nota2geo = document.querySelector("#nota2geo")
+mediageo = document.querySelector("#mediageo")
+
+//Médias gerais
+mediatotalexatas = document.querySelector("#mediatotalexatas")
+mediatotalhumanas = document.querySelector("#mediatotalhumanas")
